4d1314526da6ff323363368ca6e052163ce29622
Nick Weber <webernick@icloud.com>
2018-10-19 09:12:33 -0700

    great sorting tutorial

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,22 @@
+class ProductsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
+  def index
+    @products = Product.order("#{sort_column} #{sort_direction} ")
+  end
+
+private
+
+def sortable_columns
+  ["name", "price"]
+end
+
+def sort_column
+  sortable_columns.include?(params[:column]) ? params[:column] : "name"
+end
+
+def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+Product.create!(id: 1, name: "Banana", price: "1.00")
+Product.create!(id: 2, name: "Apple", price: "1.50")
+Product.create!(id: 3, name: "Grape", price: "0.10")
+Product.create!(id: 4, name: "Strawberry", price: "0.30")
+Product.create!(id: 5, name: "Blueberry", price: "0.50")
+Product.create!(id: 6, name: "Raspberry", price: "0.33")
